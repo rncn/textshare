977ff2146335530ba75bbcf1b875ae1a285afedb
rncn <doraidamon1rwfo@outlook.jp>
2020-10-06 22:31:03 +0900

    js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,6 @@
             const input = document.forms.main.content;
             input.value = container;
             document.forms.main.submit();
-            window.onbeforeunload = function(e) {
-            e.returnValue = "ページを離れようとしています。よろしいですか？";
-            }
         }
     </script>
 
